1b4c0760525345d3fd85ddb578e8c7193a7a6f18
Boris Osechinskiy <osechinskiy@gmail.com>
2017-04-11 23:01:06 +0300

    Написал функцию для проверки типов жилья

--- js/map.js
+++ js/map.js
@@ -1,6 +1,14 @@
+// 1 фичи(спан) добавлять в фрагмент с помощью функции и возвращать фрагмент и дальше с ним рабоать
+//
+
 'use strict';
   //  1.Создайте массив, состоящий из 8 сгенерированных JS объектов,
   //  которые будут описывать похожие объявления неподалеку. Структура объектов должна быть следующей:
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+  }
+
 function getObject(n) {
   var objects = [];
   var title = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
@@ -10,14 +18,14 @@ function getObject(n) {
   var photos = [];
   var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   var maxSteep = features.length;
+
   for (var i = 0; i < n; i++) {
-    var priceRand = 1000 + Math.floor(Math.random() * (1000000 - 1000 + 1));
-    var titleRand = Math.floor(Math.random() * (title.length));
-    var locationRandX = 300 + Math.floor(Math.random() * (900 - 300 + 1));
-    var locationRandY = 100 + Math.floor(Math.random() * (500 - 100 + 1));
+    var priceRand = getRandomInt(1000, 1000000);
+    var locationRandX = getRandomInt(300, 900);
+    var locationRandY = getRandomInt(100, 500);
     var typeRand = Math.floor(Math.random() * (type.length));
-    var roomsRand = 1 + Math.floor(Math.random() * (5));
-    var guestsRand = 5 + Math.floor(Math.random() * (100 - 5 + 1));
+    var roomsRand = getRandomInt(1, 5);
+    var guestsRand = getRandomInt(1, 20);
     var checkinRand = Math.floor(Math.random() * (checkin.length));
     var checkoutRand = Math.floor(Math.random() * (checkout.length));
     var firtsSteep = Math.floor(Math.random() * (maxSteep - 3 + 1)) + 0;
@@ -30,7 +38,7 @@ function getObject(n) {
       },
 
       offer: {
-        title: title[titleRand],
+        title: title[i],
         address: locationRandX + ', ' + locationRandY,
         price: priceRand,
         type: type[typeRand],
@@ -71,40 +79,47 @@ for (var i = 0; i < objects.length; i++) {
   newFragment.appendChild(newElement);
 }
 
-// На основе первого по порядку элемента из сгенерированного массива и шаблона #lodge-template создайте
+// 4.На основе первого по порядку элемента из сгенерированного массива и шаблона #lodge-template создайте
 // DOM-элемент, заполните его данными из объекта и вставьте полученный DOM-элемент вместо блока .dialog__panel:
 
 var lodgeTemplate = document.querySelector('#lodge-template').content;
 
-function getFeaturesType(object) {
-  var featuresType = object.offer.features;
+function getFeaturesType(local) {
+  var featuresType = local.offer.features;
   var result = [];
+var features = document.querySelector('.lodge__features');
   for (i = 0; i < featuresType.length; i++) {
     var span = document.createElement('span');
     span.classList.add('feature__image', 'feature__image--' + featuresType[i]);
-    result.push(span);
+    features.appendChild(span);
+    // result.push(span);
   }
-  return result;
+  // return result;
 }
 
-function getHouseType(house) {
-  var type = '';
-  for (i = 0; i < house.length; i++) {
-    if (house[i].offer.type === 'flat') {
-      type = 'Квартира';
-    }
-    if (house[i].offer.type === 'bungalo') {
-      type = 'Бунгало';
-    }
-    if (house[i].offer.type === 'house') {
-      type = 'Дом';
-    }
-  }
-  return type;
+function getHouseType(local) {
+  var type = local.offer.type;
+  var typeName = '';
+  switch (type) {
+  case "flat":
+          typeName = 'Квартира';
+    break;
+  case "house":
+          typeName =  'Дом'
+    break;
+  case "bungalo":
+          typeName = 'Бунгало'
+    break;
+  default:
+    console.log('Нет подходящих типов');
 }
-var houseType = getHouseType(objects);
-for (i = 0; i < objects.length; i++) {
+  return typeName;
+}
+debugger;
+var i = 0;
+//for (i = 0; i < objects.length; i++) {
   var FeaturesType = getFeaturesType(objects[i]);
+  var houseType = getHouseType(objects[i]);
   var objectElement = lodgeTemplate.cloneNode(true);
   objectElement.querySelector('.lodge__title').textContent = objects[i].offer.title;
   objectElement.querySelector('.lodge__address').textContent = objects[i].offer.address;
@@ -112,7 +127,7 @@ for (i = 0; i < objects.length; i++) {
   objectElement.querySelector('.lodge__type').textContent = houseType;
   objectElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + objects[i].offer.guests + ' гостей в ' + objects[i].offer.rooms + ' комнатах';
   objectElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + objects[i].offer.checkin + ', выезд до ' + objects[i].offer.checkout;
-  objectElement.querySelector('.lodge__features').textContent = FeaturesType;
+//  objectElement.querySelector('.lodge__features').textContent = FeaturesType;
   objectElement.querySelector('.lodge__description').textContent = objects[i].offer.description;
 
 
@@ -122,4 +137,4 @@ for (i = 0; i < objects.length; i++) {
   var dialogElement = document.querySelector('.dialog__title');
   dialogElement.innerHTML = '<img src="' + objects[i].author.avatar + '" alt="Avatar" width="70" height="70">';
   dialogElement.appendChild(objectElement);
-}
+//}
