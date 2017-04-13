3cf1dc7201bed2cc3cec4a01d61175c719a86f40
Boris Osechinskiy <osechinskiy@gmail.com>
2017-04-13 12:58:56 +0300

    Поправил замечания

--- js/map.js
+++ js/map.js
@@ -2,28 +2,23 @@
   //  1.Создайте массив, состоящий из 8 сгенерированных JS объектов,
   //  которые будут описывать похожие объявления неподалеку. Структура объектов должна быть следующей:
 
-function getRandomInt(min, max) {
+function generateRandomInt(min, max) {
   return Math.floor(Math.random() * (max + 1 - min)) + min;
 }
 
-function getRandomFeatures() {
+function generateRandomFeatures() {
   var randFeatures = [];
   var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var size = features.length;
-  for (var i = 0; i < size; i++) {
-    var randNumber = getRandomInt(0, 1);
-    var randNumberArr = getRandomInt(0, features.length - 1);
-    if (randNumber === 1) {
-      randFeatures.push(features[randNumberArr]);
-      features.splice(randNumberArr, 1);
-
+  for (var i = 0; i < features.length; i++) {
+    if (generateRandomInt(0, 1)) {
+      randFeatures.push(features[i]);
     }
   }
   return randFeatures;
 }
 
-function getObject(n) {
-  var objects = [];
+function generateAds(n) {
+  var ads = [];
   var title = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
   var type = ['flat', 'house', 'bungalo'];
   var checkin = ['12:00', '13:00', '14:00'];
@@ -31,69 +26,68 @@ function getObject(n) {
   var photos = [];
 
   for (var i = 0; i < n; i++) {
-    objects.push({
+    ads.push({
       author: {
         avatar: ('img/avatars/user0') + (i + 1) + ('.png')
       },
 
       offer: {
         title: title[i],
-        address: getRandomInt(300, 900) + ', ' + getRandomInt(100, 500),
-        price: getRandomInt(1000, 1000000),
-        type: type[getRandomInt(0, type.length - 1)],
-        rooms: getRandomInt(1, 20),
-        guests: getRandomInt(1, 20),
-        checkin: checkin[getRandomInt(0, checkin.length - 1)],
-        checkout: checkout[getRandomInt(0, checkout.length - 1)],
-        features: getRandomFeatures(),
+        address: generateRandomInt(300, 900) + ', ' + generateRandomInt(100, 500),
+        price: generateRandomInt(1000, 1000000),
+        type: type[generateRandomInt(0, type.length - 1)],
+        rooms: generateRandomInt(1, 20),
+        guests: generateRandomInt(1, 20),
+        checkin: checkin[generateRandomInt(0, checkin.length - 1)],
+        checkout: checkout[generateRandomInt(0, checkout.length - 1)],
+        features: generateRandomFeatures(),
         description: ' ',
         photos: photos
       },
 
       location: {
-        x: getRandomInt(300, 900),
-        y: getRandomInt(100, 500)
+        x: generateRandomInt(300, 900),
+        y: generateRandomInt(100, 500)
       }
     }
   );
   }
-  return objects;
+  return ads;
 }
-var objects = getObject(8);
+var ads = generateAds(8);
 
 // 2.На основе данных, созданных в предыдущем пункте создайте DOM-элементы,
 // соответствующие меткам на карте случайно сгенерированных объявлений
 // и заполните их данными из массива. Итоговая разметка метки должна выглядеть следующим образом:
-
-for (var i = 0; i < objects.length; i++) {
+var fragment = document.createDocumentFragment();
+var newFragment = document.querySelector('.tokyo__pin-map');
+for (var i = 0; i < ads.length; i++) {
   var newElement = document.createElement('div');
   newElement.className = 'pin';
-  newElement.style.left = objects[i].location.x + 'px';
-  newElement.style.top = objects[i].location.y + 'px';
-  newElement.innerHTML = '<img src="' + objects[i].author.avatar + '" class="rounded" width="40" height="40">';
+  newElement.style.left = ads[i].location.x + 'px';
+  newElement.style.top = ads[i].location.y + 'px';
+  newElement.innerHTML = '<img src="' + ads[i].author.avatar + '" class="rounded" width="40" height="40">';
 
 // 3.Отрисуйте сгенерированные DOM-элементы в блок .tokyo__pin-map. Для вставки элементов используйте DocumentFragment.
-
-  var newFragment = document.querySelector('.tokyo__pin-map');
-  newFragment.appendChild(newElement);
+  fragment.appendChild(newElement);
+  newFragment.appendChild(fragment);
 }
 
 // 4.На основе первого по порядку элемента из сгенерированного массива и шаблона #lodge-template создайте
 // DOM-элемент, заполните его данными из объекта и вставьте полученный DOM-элемент вместо блока .dialog__panel:
 
-var lodgeTemplate = document.querySelector('#lodge-template').content;
-
-function getFeaturesType(local) {
+function generateFeaturesType(local) {
   var featuresType = local.offer.features;
-  var features = document.querySelector('.lodge__features');
+  var fragmentFeatures = document.createDocumentFragment();
   for (i = 0; i < featuresType.length; i++) {
     var span = document.createElement('span');
     span.classList.add('feature__image', 'feature__image--' + featuresType[i]);
-    features.appendChild(span);
+    fragmentFeatures.appendChild(span);
   }
+  return fragmentFeatures;
 }
 
-function getHouseType(local) {
+function generateHouseType(local) {
   var type = local.offer.type;
   var typeName = '';
   switch (type) {
@@ -110,23 +104,34 @@ function getHouseType(local) {
   return typeName;
 }
 
+function replaceElement(newChild, oldEChild) {
+  var parent = oldEChild.parentElement;
+  parent.replaceChild(newChild, oldEChild);
+}
 i = 0;
-var FeaturesType = getFeaturesType(objects[i]);
-var houseType = getHouseType(objects[i]);
-var objectElement = lodgeTemplate.cloneNode(true);
-objectElement.querySelector('.lodge__title').textContent = objects[i].offer.title;
-objectElement.querySelector('.lodge__address').textContent = objects[i].offer.address;
-objectElement.querySelector('.lodge__price').textContent = objects[i].offer.price + '&#x20bd;/ночь';
-objectElement.querySelector('.lodge__type').textContent = houseType;
-objectElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + objects[i].offer.guests + ' гостей в ' + objects[i].offer.rooms + ' комнатах';
-objectElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + objects[i].offer.checkin + ', выезд до ' + objects[i].offer.checkout;
-objectElement.querySelector('.lodge__features').textContent = FeaturesType;
-objectElement.querySelector('.lodge__description').textContent = objects[i].offer.description;
+var featuresType = generateFeaturesType(ads[i]);
+var houseType = generateHouseType(ads[i]);
+
+function renderDialogPanel(local) {
+  var lodgeTemplate = document.querySelector('#lodge-template').content;
+  var adsElement = lodgeTemplate.cloneNode(true);
+
+  adsElement.querySelector('.lodge__title').textContent = local.offer.title;
+  adsElement.querySelector('.lodge__address').textContent = local.offer.address;
+  adsElement.querySelector('.lodge__price').innerHTML = local.offer.price + '&#x20bd;/ночь';
+  adsElement.querySelector('.lodge__type').textContent = houseType;
+  adsElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + local.offer.guests + ' гостей в ' + local.offer.rooms + ' комнатах';
+  adsElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + local.offer.checkin + ', выезд до ' + local.offer.checkout;
+  adsElement.querySelector('.lodge__features').appendChild(featuresType);
+  adsElement.querySelector('.lodge__description').textContent = local.offer.description;
 
 
-var dialogPanelElement = document.querySelector('.dialog__panel');
-dialogPanelElement.innerHTML = objectElement;
+  var dialogPanelElement = document.querySelector('.dialog__panel');
+  replaceElement(adsElement, dialogPanelElement);
 
-var dialogElement = document.querySelector('.dialog__title');
-dialogElement.innerHTML = '<img src="' + objects[i].author.avatar + '" alt="Avatar" width="70" height="70">';
-dialogElement.appendChild(objectElement);
+  var dialogElement = document.querySelector('.dialog__title');
+  dialogElement.getElementsByTagName('img')[0].src = ads[i].author.avatar;
+
+}
+
+renderDialogPanel(ads[0]);
